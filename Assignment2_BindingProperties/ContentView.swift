f1538a9412c16a4f96474b0e1ed6517661f12491
cyanYellow <williecyanyellow@gmail.com>
2023-09-08 21:19:58 -0600

    Update

--- Assignment2_BindingProperties/ContentView.swift
+++ Assignment2_BindingProperties/ContentView.swift
@@ -9,13 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        
+            .frame(width: 500, height: 500, alignment: .center)
+            .overlay(
+                Text("Willie Green")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .frame( width: 500, height: 50)
+                    .background(
+                        Rectangle()
+                            .fill(Color.white))
+            )
+        Menu("portfolio") {
+            Text("Web Design")
+            Text("Experience Design")
+            Text("Graphic Design")
+            }
+            .frame(width: 500, height: 50, alignment: .center)
+            .background(
+                Rectangle()
+                    .fill(Color.white))
+        Form(content: {
+            Text("get in touch")
+            TextField("Name", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+            TextField("email", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+        })
+        .padding(.top, 15.0)
     }
 }
 
