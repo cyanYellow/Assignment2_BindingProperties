ca5a6cbeaa195764447ee0f890fc3dba1a536bc9
cyanYellow <williecyanyellow@gmail.com>
2023-09-08 20:35:25 -0600

    Initial Commit

---
+++ Assignment2_BindingProperties/Assignment2_BindingPropertiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment2_BindingPropertiesApp.swift
+//  Assignment2_BindingProperties
+//
+//  Created by Willie Green on 9/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Assignment2_BindingPropertiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
